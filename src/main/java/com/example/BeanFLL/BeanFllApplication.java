94b178ada9f48b95504a034d490395899bb79de6
surabhi <surabhi.das@outlook.com>
2023-08-17 14:29:45 -0400

    starting lab

---
+++ src/main/java/com/example/BeanFLL/BeanFllApplication.java
@@ -0,0 +1,13 @@
+package com.example.BeanFLL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanFllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanFllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BeanFLL/BeanFllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeanFLL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanFllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
